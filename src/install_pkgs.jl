53e2e461c3e0ad8d6c08207b95d87f889d8728f2
dipanjana-dalui <31413248+dipanjana-dalui@users.noreply.github.com>
2025-09-12 13:19:22 -0500

    Create install_pkgs.jl

---
+++ src/install_pkgs.jl
@@ -0,0 +1,16 @@
+using Pkg
+
+Pkg.add("DifferentialEquations")
+Pkg.add("Plots")
+Pkg.add("Distributions")
+Pkg.add("DataFrames")
+Pkg.add("Random")
+Pkg.add("Statistics")
+Pkg.add("StaticArrays")
+Pkg.add("StatsBase")
+Pkg.add("AlgebraOfGraphics")
+Pkg.add("CSV")
+Pkg.add("Makie")
+Pkg.add("CairoMakie")
+Pkg.add("Tidier")
+Pkg.add("UnPack")
